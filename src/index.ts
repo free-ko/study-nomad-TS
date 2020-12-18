e1a3389a40fcef86d3ecf4cef67286c442719855
free-ko <duddnr654@naver.com>
2020-12-18 22:19:34 +0900

    #5 Types in Typescript

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you ar ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi("FREEKO", 29, "male"));
+
+export{};
